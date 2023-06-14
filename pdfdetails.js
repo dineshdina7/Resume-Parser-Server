e3578a4277bcd954375cab154ec18df758b833f6
dineshdina7 <dineshdinaa777@gmail.com>
2023-06-14 20:53:45 +0530

    Database Connected

---
+++ pdfdetails.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const pdfSchema = new mongoose.Schema(
+	{
+		
+		pdf: String,
+	},
+	{
+		collection: "PdfDetails",
+	}
+);
+
+mongoose.model("PdfDetails", pdfSchema);
